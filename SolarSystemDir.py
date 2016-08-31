83f4d2f5f638fc757f6b1107f8384238509ba852
iamsabhoho <sabrinasj.ho@gmail.com>
2016-08-31 10:13:25 +0800

    updated the repository and journal

---
+++ SolarSystemDir.py
@@ -0,0 +1,11 @@
+'''This program show s teh size and distances of the planes in hte solar system with reference to the earth
+    
+    User enters the planet name.
+'''
+
+planet = input('Enter the name of the planet(Venus, Mars, Jupiter, Neptune, Saturn, Mercury)= ')
+
+if planet != 'Venus' != 'Mars'!='Jupiter'!='Neptune'!='Saturn'!='Mercury':
+    print('This is not a planet')
+else:
+    print('a planet')
