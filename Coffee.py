a91de1a5ccad0e85d5894b3eaee34024de3f5958
iamsabhoho <sabrinasj.ho@gmail.com>
2016-09-26 23:54:56 +0800

    Updated test

---
+++ Coffee.py
@@ -0,0 +1,77 @@
+from time import sleep
+import signal
+
+TIMEOUT = 10
+TIMEOUT2 = 0
+p = 0
+time = 0
+LS = 20
+LB = 30
+AS = 30
+AB = 50
+ES = 25
+EB = 40
+while True:
+    #Ordering coffee
+    print('This are the three types of coffee: ')
+    print('1 - Latte')
+    print('2 - Americano')
+    print('3 - Expresso')
+    c = int(input('What type of coffee would you like to order?(Type in the number): '))
+
+    print('This are the sizes for coffee: ')
+    print('1 - BIG')
+    print('2 - SMALL')
+    size = int(input('What size of coffee would you like?(Type in the number): '))
+
+    if c == 1:
+        ptime = 6
+        if size == 1:
+            p = LB
+        elif size == 2:
+            p = LS
+        else:
+            print('Please order again.')
+            break
+    elif c == 2:
+        ptime = 4
+        if size == 1:
+            p = AB
+        elif size == 2:
+            p = AS
+        else:
+            print('Please order again.')
+            break
+    elif c == 3:
+        ptime = 5
+        if size == 1:
+            p = EB
+        elif size == 2:
+            p = ES
+        else:
+            print('Please order again.')
+            break
+    else:
+        print('Please order again.')
+
+
+    s = int(input('How many sugar would you like?(0-5): '))
+    ps = s*1
+    t = p + ps
+    print('The total price is: ' + str(t))
+
+    print('Would you like to start or cancel?(START/CANCEL): ')
+
+    signal.alarm(TIMEOUT)
+    sin = input()
+    signal.alarm(0) # disable the alarm after success
+
+    if sin == 'START':
+        print('The time required for preparation is: ' + str(ptime) + ' s.')
+        TIMEOUT2 = ptime
+        signal.alarm(TIMEOUT2)
+        signal.alarm(0)
+        if TIMEOUT2 == 0:
+            print('READY')
+    else:
+        continue
